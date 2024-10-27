05406a10d48686aab66ed12ceb49755609e6f6d7
DhruPrajapati <76686616+DhruPrajapati@users.noreply.github.com>
2024-10-27 15:29:52 -0400

    displaying critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -8,11 +8,12 @@ export interface Platform {
     slug: string;
 }
 
-export interface Game {
+export interface Game { 
   id: number;
   name: string;
   background_image: string;
-  parent_platforms : {platform : Platform}[]
+  parent_platforms : {platform : Platform}[] 
+  metacritic: number
 }
 
 interface FetchGamesResponse {
