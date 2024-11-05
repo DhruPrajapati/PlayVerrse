1e81037510f429ea70b99938071f1920ec530df8
DhruPrajapati <76686616+DhruPrajapati@users.noreply.github.com>
2024-11-04 23:20:44 -0500

    implement infinite query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -17,7 +17,8 @@ const useGenres = () =>
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24 hours
-    initialData: { count: genres.length, results: genres },
+    initialData: { count: genres.length, next: null, results: genres },
   });
-
-export default useGenres;
+  
+  export default useGenres;
+  
