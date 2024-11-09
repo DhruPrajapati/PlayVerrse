eae73653a2e6964cd6c790326154b07727b28e26
DhruPrajapati <76686616+DhruPrajapati@users.noreply.github.com>
2024-11-09 16:44:27 -0500

    fetch games Details

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,10 +4,19 @@ import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 import Emoji from "./Emoji";
+import { Link } from "react-router-dom";
 
 const GameCard = ({ game }: { game: Game }) => {
   return (
-    <Card borderRadius={10} overflow="hidden" width="300">
+    <Card
+      borderRadius={10}
+      overflow="hidden"
+      width="300"
+      transition="transform 0.2s ease-in-out, box-shadow 0.2s ease-in-out"
+      _hover={{
+        transform: "scale(1.05)",
+        boxShadow: "lg",
+      }}>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <HStack justifyContent="space-between" marginBottom={3}>
@@ -17,7 +26,8 @@ const GameCard = ({ game }: { game: Game }) => {
           <CriticScore score={game.metacritic} />
         </HStack>
         <Heading fontSize="2xl">
-          {game.name} <Emoji rating={game.rating_top} />
+          <Link to={"/games/" + game.slug}> {game.name}</Link>{" "}
+          <Emoji rating={game.rating_top} />
         </Heading>
       </CardBody>
     </Card>
